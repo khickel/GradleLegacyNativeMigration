313ba218ed42b80e0a3580812a36dd40ef8c0b32
Kelly Hickel <Kelly_Hickel@bmc.com>
2020-07-29 07:11:48 -0500

    Added another subproject, some cleanup

---
+++ subsystem_b/server_1/server_1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lib_one.h"
+
+int main(int argc, char *argv[]) {
+  printf("server1_1.cpp calling lib_one_func_one three times.\n");
+  lib_one_func_one(3, "server_1");
+}
