78387ceaa2ea86903e75f0c224678bc40b5e84a3
Kelly Hickel <Kelly_Hickel@bmc.com>
2020-09-14 14:38:27 -0500

    Added example showing the downside of gradle forcing /TP on all visual studio C/C++ compilations.

---
+++ subsystem_a/util_1/c_only.c
@@ -0,0 +1,13 @@
+#include "lib_one.h"
+
+#include "Camelcase.h"
+
+int c_only(int i) {
+
+  // This is a contrived example, but there are .c files that cannot correctly be compiled in c++ mode.
+  // See comment in util_1.gradle for a workaround that involves building your own gradle.
+#ifdef __cplusplus
+#error This file must be compiled as a C file, not a C++ file.
+#endif
+  return 0;
+}
