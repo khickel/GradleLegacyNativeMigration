3af1acb81c6c2e3f98a1ce313053c67b28d28ae8
Kelly Hickel <Kelly_Hickel@bmc.com>
2020-11-19 14:59:27 -0600

    Added a mixed language static library, now util_1 has a link problem.

---
+++ static_lib/lib_one_pp.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <stdio.h>
+#include <direct.h>
+#include <io.h>
+#include <time.h>
+
+#include "lib_One.h"
+#include "CamelCase.h"
+
+int lib_one_cpp_func_one(int i) {
+  return i*2;
+}
