d937afa46709b8210b38e1f96b6094451f45a114
Kelly Hickel <Kelly_Hickel@bmc.com>
2021-01-27 16:53:52 -0600

    Building on Linux
    	modified:   build.gradle
    	modified:   commonNativeUtils.gradle
    	modified:   gradlew
    	modified:   inst_three/inst_three.gradle
    	modified:   installUtils.gradle
    	modified:   settings.gradle
    	new file:   staging/server_1
    	modified:   static_lib/lib_one.c
    	modified:   static_lib/static_lib.gradle
    	modified:   subsystem_a/util_1/util_1.cpp
    	modified:   subsystem_b/server_1/server_1.cpp

--- subsystem_a/util_1/util_1.cpp
+++ subsystem_a/util_1/util_1.cpp
@@ -1,12 +1,22 @@
+
+#ifdef WIN32
 #include <windows.h>
-#include <stdio.h>
 #include <direct.h>
 #include <io.h>
+#endif
+
+#include <stdio.h>
 #include <time.h>
 
 #include "lib_one.h"
 
+#ifdef WIN32
+// Illustrate the gradle issue on windows where the case of the filename in the
+// include statement doesn't match the case on disk.
 #include "Camelcase.h"
+#else
+#include "CamelCase.h"
+#endif
 
 int main(int argc, char *argv[]) {
   printf("util_1.cpp calling lib_one_func_one twice.\n");
