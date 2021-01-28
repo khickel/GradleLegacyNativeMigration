b7456c590e847ef8ff615b3ba43fdd0b4f6cff08
Kelly Hickel <Kelly_Hickel@bmc.com>
2021-01-27 19:25:18 -0600

    Code review fixes.

--- subsystem_a/util_1/util_1.cpp
+++ subsystem_a/util_1/util_1.cpp
@@ -13,8 +13,12 @@
 #ifdef WIN32
 // Illustrate the gradle issue on windows where the case of the filename in the
 // include statement doesn't match the case on disk.
+// This problem only happens on windows, some of the Visual Studio header files contain #includes
+// for other VS headers and the case doesn't match the actual filename, which can cause gradle to
+// keep rebuilding dependencies over and over, each time you run the command.
 #include "Camelcase.h"
 #else
+// Use the correct case on Linux, otherwise the file can't be found.
 #include "CamelCase.h"
 #endif
 
